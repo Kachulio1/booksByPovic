7430e4741a144cf049cc026f9eb948c6022ddb31
kachulio1 <jngugi88@gmail.com>
2018-12-17 02:08:02 +0300

    add coveralls

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,12 @@ module.exports = {
   verbose: true,
   collectCoverage:true,
   setupTestFrameworkScriptFile: "<rootDir>/src/tests/setupTests.js",
+  coverageReporters:["lcov", "text"],
   collectCoverageFrom: [
     "**/*.{js,jsx}",
     "!**/node_modules/**",
-    "!**/vendor/**"
+    "!**/vendor/**",
+    "!**/coverage/**",
+    "!**/src/index.js"
   ]
 }
